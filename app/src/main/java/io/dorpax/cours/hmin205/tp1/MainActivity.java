09d7cc97f8bbcfb1da176612e7ff046483fdc56c
Charles DESGENETEZ <dorpaxione@gmail.com>
2021-02-01 15:31:32 +0100

    fini

--- app/src/main/java/io/dorpax/cours/hmin205/tp1/MainActivity.java
+++ app/src/main/java/io/dorpax/cours/hmin205/tp1/MainActivity.java
@@ -18,22 +18,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        ConstraintLayout layout = findViewById(R.id.main_layout);
-        for (int i = 0; i < layout.getChildCount(); i++) {
-            View child = layout.getChildAt(i);
-            System.out.println(child);
-            if(child instanceof EditText) {
-                ((EditText) child).setText("");
-            }
-        }
+        final ConstraintLayout layout = findViewById(R.id.main_layout);
         new AlertDialog.Builder(this)
                 .setTitle(R.string.dialog_validation_title)
                 .setMessage(R.string.dialog_validation_message)
                 .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
+                        for (int i = 0; i < layout.getChildCount(); i++) {
+                            View child = layout.getChildAt(i);
+                            System.out.println(child);
+                            if(child instanceof EditText) {
+                                EditText et = (EditText) child;
+                                et.setBackground(new EditText(getBaseContext()).getBackground());
+                                et.setText("");
+                            }
+                        }
                     }
                 })
-                .show();
+                .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        for (int i = 0; i < layout.getChildCount(); i++) {
+                            View child = layout.getChildAt(i);
+                            System.out.println(child);
+                            if(child instanceof EditText) {
+                                ((EditText) child).setBackgroundColor(R.color.colorAccent);
+                            }
+                        }
+                    }
+                }).show();
     }
 }
