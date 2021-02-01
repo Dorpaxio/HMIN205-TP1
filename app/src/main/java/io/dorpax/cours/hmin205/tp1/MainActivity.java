d1a0cd35e23499f9c4ea7a7419808e0bc5083a0a
Charles DESGENETEZ <dorpaxione@gmail.com>
2021-02-01 19:00:51 +0100

    remove top bar

--- app/src/main/java/io/dorpax/cours/hmin205/tp1/MainActivity.java
+++ app/src/main/java/io/dorpax/cours/hmin205/tp1/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
